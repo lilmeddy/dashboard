c92a6133d6c63eb69326bdaf529ecdd6c6512fc4
Osamede <aghedopriscilla@gmail.com>
2023-07-14 22:46:58 +0100

    done with first 2 html

--- public/log.js
+++ public/log.js
@@ -2,3 +2,22 @@ function authenticate() {
   localStorage.setItem("isAuthenticated", "true");
   window.location.href = "dash.html";
 }
+let email = document.getElementById("email")
+  // let phone = document.getElementById('phone')
+let password = document.getElementById("password")
+function sign(ev){
+
+  ev.preventDefault()
+  firebase.auth().signInWithEmailAndPassword(email.value, password.value)
+  .then((userCredential) => {
+    // Signed in
+    var user = userCredential.user;
+    alert("success")
+    // ...
+  })
+  .catch((error) => {
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    alert("invalid")
+  });
+    }
