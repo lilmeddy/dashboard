1c4e4bd62c82e74b090b5301e99ea03f70605df5
Osamede <aghedopriscilla@gmail.com>
2023-07-17 09:38:54 +0100

    trying the blog

--- public/dash.js
+++ public/dash.js
@@ -1,57 +1,101 @@
-let posts = JSON.parse(localStorage.getItem('posts')) || [];
-function renderContent(content) {
-    let contentContainer = document.getElementById('contentContainer');
-     contentContainer.innerHTML = content;
-   }
-   function renderContent(content) {
-    let contentContainer = document.getElementById('contentContainer');
-     contentContainer.innerHTML = content;
-   }
-   function showCreatePostForm() {
-    let createPostForm = document.getElementById('createPostForm');
-     createPostForm.style.display = 'block';
+// let posts = JSON.parse(localStorage.getItem('posts')) || [];
 
-     renderContent('');
-   }
-   function showAllPosts() {
-    if (posts.length > 0) {
-     let content = posts
-        .map(post => `
-          <div class="post">
-            <h3>${post.title}</h3>
-            <p>${post.content}</p>
-          </div>
-        `)
-        .join('');
+// function renderContent() {
+//   let contentContainer = document.getElementById('contentContainer');
+//    contentContainer.innerHTML = '';
+ 
+// posts.forEach((post, index) => {
+//   let postElement = document.createElement('div');
+//   postElement.classList.add('post');
+//   postElement.innerHTML = `
+//     <h3>${post.title}</h3>
+//     <p>${post.content}</p>
+//     <div class="btn-group">
+//       <button onclick="editPost(${index})">Edit</button>
+//       <button onclick="deletePost(${index})">Delete</button>
+//       <button onclick="toggleLike(${index})" class="${post.liked ? 'liked' : ''}">
+//         ${post.liked ? 'Unlike' : 'Like'}
+//       </button>
+//     </div>
+//   `;
+ 
+//   contentContainer.appendChild(postElement);
+  
+// });
+// function createPost(event) {
+//   event.preventDefault();
 
-      renderContent(content);
-    } else {
-      renderContent('<p>No posts found.</p>');
-    }
-  }
-  function createPost(event) {
-    event.preventDefault();
+//   let titleInput = document.getElementById('title');
+//   let contentInput = document.getElementById('content');
 
-   let titleInput = document.getElementById('title');
-   let contentInput = document.getElementById('content');
+//   let post = {
+//     title: titleInput.value,
+//     content: contentInput.value,
+//     liked: false
+//   };
 
-   let post = {
-      title: titleInput.value,
-      content: contentInput.value,
-      liked: false
-    };
+//   posts.push(post);
+//   localStorage.setItem('posts', JSON.stringify(posts));
 
-    posts.unshift(post);
-    localStorage.setItem('posts', JSON.stringify(posts));
+//   titleInput.value = '';
+//   contentInput.value = '';
 
-    titleInput.value = '';
-    contentInput.value = '';
+//   renderPosts();
+// }
+// }
+// function showCreatePostForm() {
+//     let createPostForm = document.getElementById('createPostForm');
+//      createPostForm.style.display = 'block';
+//     //  createPost(event)
+    
+//    }
 
-    showAllPosts();
 
-   let createPostForm = document.getElementById('createPostForm');
-    createPostForm.style.display = 'none';
-  }
-  let postForm = document.getElementById('postForm');
-  postForm.addEventListener('submit', createPost);
-  showAllPosts();
+  //  function showCreatePostForm() {
+  //   let createPostForm = document.getElementById('createPostForm');
+  //    createPostForm.style.display = 'block';
+
+  //    renderContent('');
+  //  }
+  //  function showAllPosts() {
+  //   if (posts.length > 0) {
+  //    let content = posts
+  //       .map(post => `
+  //         <div class="post">
+  //           <h3>${post.title}</h3>
+  //           <p>${post.content}</p>
+  //         </div>
+  //       `)
+  //       .join('');
+
+  //     renderContent(content);
+  //   } else {
+  //     renderContent('<p>No posts found.</p>');
+  //   }
+  // }
+  // function createPost(event) {
+  //   event.preventDefault();
+
+  //  let titleInput = document.getElementById('title');
+  //  let contentInput = document.getElementById('content');
+
+  //  let post = {
+  //     title: titleInput.value,
+  //     content: contentInput.value,
+  //     liked: false
+  //   };
+
+  //   posts.unshift(post);
+  //   localStorage.setItem('posts', JSON.stringify(posts));
+
+  //   titleInput.value = '';
+  //   contentInput.value = '';
+
+  //   showAllPosts();
+
+  //  let createPostForm = document.getElementById('createPostForm');
+  //   createPostForm.style.display = 'none';
+  // }
+  // let postForm = document.getElementById('postForm');
+  // postForm.addEventListener('submit', createPost);
+  // showAllPosts();
